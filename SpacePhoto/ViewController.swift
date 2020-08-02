b5a0169bde2fa17bba3cc19ab06fdc0e13a68f25
Armando Carrillo <ingcompuarmando@gmail.com>
2020-08-02 14:33:27 -0500

    Make the network request

--- SpacePhoto/ViewController.swift
+++ SpacePhoto/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoFromAPOD: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var CopyrightLabel: UILabel!
+    
+   let photoInfoController = PhotoInfoController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        descriptionLabel.text = ""
+        CopyrightLabel.text = ""
+        
+        photoInfoController.fetchPhotoInfo { (photoInfo) in
+            if let photoInfo = photoInfo {
+                self.title = photoInfo.title
+                self.descriptionLabel.text = photoInfo.description
+            
+            if let copyright = photoInfo.copyright {
+                self.CopyrightLabel.text = "Copyright \(copyright)"
+            } else {
+                self.CopyrightLabel.isHidden = true
+            }
+        }
     }
 
 
 }
-
+}
