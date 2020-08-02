3ff77472fec34a2ba9847dcc2d66ecceb16bd018
Armando Carrillo <ingcompuarmando@gmail.com>
2020-08-02 13:43:05 -0500

    Initial Commit

---
+++ SpacePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Armando Carrillo on 02/08/20.
+//  Copyright © 2020 Armando Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
