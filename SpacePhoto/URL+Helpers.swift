8831423727797fc091d0ff93deb173db5d97472a
Armando Carrillo <ingcompuarmando@gmail.com>
2020-08-04 14:37:55 -0500

    App transport security and the http protocol

--- SpacePhoto/URL+Helpers.swift
+++ SpacePhoto/URL+Helpers.swift
@@ -15,4 +15,13 @@ extension URL{
             URLQueryItem(name: $0.0, value: $0.1) }
         return components?.url
     }
+   
+    func withHTTPS() -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.scheme = "https"
+        return components?.url
+    }
+
+    
 }
+
